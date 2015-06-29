50f491ade0b8b51c487ff2cf48fed42a9b822d41
solene <solene.dardennes@edu.esiee.fr>
2015-06-29 20:00:46 +0200

    Browser partiellement implémenté (no errors)

--- app/src/main/java/com/sopy/mediaplayerremoteapp/KodiDiscovery.java
+++ app/src/main/java/com/sopy/mediaplayerremoteapp/KodiDiscovery.java
@@ -1,38 +1,204 @@
 package com.sopy.mediaplayerremoteapp;
 
-import android.support.v7.app.ActionBarActivity;
+
+
+/*
+This class is the "BrowserActivity" in cling tuto
+*/
+
+
+import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.IBinder;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.android.FixedAndroidLogHandler;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.registry.DefaultRegistryListener;
+import org.fourthline.cling.registry.Registry;
 
+public class KodiDiscovery extends ListActivity {
 
-public class KodiDiscovery extends ActionBarActivity {
+    private ArrayAdapter<DeviceDisplay> listAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kodi_discovery);
-    }
+    private BrowseRegistryListener registryListener = new BrowseRegistryListener();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_kodi_discovery, menu);
-        return true;
-    }
+    private AndroidUpnpService upnpService;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private ServiceConnection serviceConnection = new ServiceConnection() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            upnpService = (AndroidUpnpService) service;
+
+            // Clear the list
+            listAdapter.clear();
+
+            // Get ready for future device advertisements
+            upnpService.getRegistry().addListener(registryListener);
+
+            // Now add all devices to the list we already know about
+            for (Device device : upnpService.getRegistry().getDevices()) {
+                registryListener.deviceAdded(device);
+            }
+
+            // Search asynchronously for all devices, they will respond soon
+            upnpService.getControlPoint().search();
         }
 
-        return super.onOptionsItemSelected(item);
+        public void onServiceDisconnected(ComponentName className) {
+            upnpService = null;
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Fix the logging integration between java.util.logging and Android internal logging
+        org.seamless.util.logging.LoggingUtil.resetRootHandler(
+                new FixedAndroidLogHandler()
+        );
+        // Now you can enable logging as needed for various categories of Cling:
+        // Logger.getLogger("org.fourthline.cling").setLevel(Level.FINEST);
+
+        listAdapter = new ArrayAdapter<DeviceDisplay>(this, android.R.layout.simple_list_item_1);
+        setListAdapter(listAdapter);
+
+        // This will start the UPnP service if it wasn't already started
+        getApplicationContext().bindService(
+                new Intent(this, AndroidUpnpServiceImpl.class),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        );
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (upnpService != null) {
+            upnpService.getRegistry().removeListener(registryListener);
+        }
+        // This will stop the UPnP service if nobody else is bound to it
+        getApplicationContext().unbindService(serviceConnection);
+    }
+
+
+
+    protected class DeviceDisplay {
+
+        Device device;
+
+        public DeviceDisplay(Device device) {
+            this.device = device;
+        }
+
+        public Device getDevice() {
+            return device;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            DeviceDisplay that = (DeviceDisplay) o;
+            return device.equals(that.device);
+        }
+
+        @Override
+        public int hashCode() {
+            return device.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            String name =
+                    getDevice().getDetails() != null && getDevice().getDetails().getFriendlyName() != null
+                            ? getDevice().getDetails().getFriendlyName()
+                            : getDevice().getDisplayString();
+            // Display a little star while the device is being loaded (see performance optimization earlier)
+            return device.isFullyHydrated() ? name : name + " *";
+        }
+    }
+
+
+
+    protected class BrowseRegistryListener extends DefaultRegistryListener {
+
+        /* Discovery performance optimization for very slow Android devices! */
+        @Override
+        public void remoteDeviceDiscoveryStarted(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceDiscoveryFailed(Registry registry, final RemoteDevice device, final Exception ex) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(
+                            KodiDiscovery.this,
+                            "Discovery failed of '" + device.getDisplayString() + "': "
+                                    + (ex != null ? ex.toString() : "Couldn't retrieve device/service descriptors"),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
+            deviceRemoved(device);
+        }
+    /* End of optimization, you can remove the whole block if your Android handset is fast (>= 600 Mhz) */
+
+        @Override
+        public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
+            deviceRemoved(device);
+        }
+
+        @Override
+        public void localDeviceAdded(Registry registry, LocalDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void localDeviceRemoved(Registry registry, LocalDevice device) {
+            deviceRemoved(device);
+        }
+
+        public void deviceAdded(final Device device) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    DeviceDisplay d = new DeviceDisplay(device);
+                    int position = listAdapter.getPosition(d);
+                    if (position >= 0) {
+                        // Device already in the list, re-set new value at same position
+                        listAdapter.remove(d);
+                        listAdapter.insert(d, position);
+                    } else {
+                        listAdapter.add(d);
+                    }
+                }
+            });
+        }
+
+        public void deviceRemoved(final Device device) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    listAdapter.remove(new DeviceDisplay(device));
+                }
+            });
+        }
+    }
+
+
+
 }
